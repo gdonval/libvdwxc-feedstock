ea2a46b0d24af006b564615af3072832130b537c
Gaël Donval <github.com@umessage.me>
2022-10-12 16:31:11 +0100

    Use workarounds for openmpi support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,14 @@
 
 mkdir build && cd build
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  # This is only used by open-mpi's mpicc
+  # ignored in other cases
+  export OMPI_CC=$CC
+  export OMPI_CXX=$CXX
+  export OMPI_FC=$FC
+  export OPAL_PREFIX=$PREFIX
+fi
 
 if [[ x"$mpi" != x"nompi" ]]; then
   ../configure --prefix=$PREFIX --disable-static CC=mpicc FC=mpifort CFLAGS="$CFLAGS -O3 -ffast-math -funroll-loops" FCFLAGS="$CFLAGS -O3 -ffast-math -funroll-loops" --with-fftw3=$PREFIX --with-mpi=$PREFIX
